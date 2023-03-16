0f57c722ef8b438aa074c8d2feee4b4ff40a65f8
mohamedkhefacha <khefachamohamed1995@gmail.com>
2023-03-16 13:49:44 -0700

    update task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(){
-	puts("\nProgramming is like building a multilingual puzzle");
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\""Programming is like building a multilingual puzzle");
 	return(0);
 }
+
