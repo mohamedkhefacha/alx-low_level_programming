fea485c7cf537320bbbfb018b5e6bb7d7c1e1e9f
mohamedkhefacha <khefachamohamed1995@gmail.com>
2023-03-16 14:54:32 -0700

    3rd update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+#include <string.h>
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fwrite( "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",1,strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"),stdout);
 	return (1);
 }
