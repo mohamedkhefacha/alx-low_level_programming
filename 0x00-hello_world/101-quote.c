c92a68ddd1916cc7d6f0b7f269ff5f779c118b61
mohamedkhefacha <khefachamohamed1995@gmail.com>
2023-03-16 15:04:25 -0700

    line long2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1
-	, strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"), stdout);
+	, strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+	, stdout);
 	return (1);
 }
